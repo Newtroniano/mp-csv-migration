98387db66a6243f18de015086179b4e816a74406
unknown <luisocara.luis@gmail.com>
2025-06-13 11:53:17 -0300

    create spring boot project

---
+++ src/main/java/com/luis/multiportal/MultiportalApplication.java
@@ -0,0 +1,13 @@
+package com.luis.multiportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luis/multiportal/MultiportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.multiportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
